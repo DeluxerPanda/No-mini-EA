2dd069effda82393097eae758e71fbb41bce50b3
DeluxPanda <github@deluxerpanda.se>
2023-09-16 21:18:48 +0200

    update

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace No_Mini_EA
@@ -8,60 +10,73 @@ namespace No_Mini_EA
     {
         private readonly NotifyIcon notifyIcon;
         private readonly StartUp startup = new StartUp();
+        private readonly String processName = "EADesktop";
         public Form1()
         {
             InitializeComponent();
             WindowState = FormWindowState.Minimized;
             notifyIcon = new NotifyIcon();
+            //   Image image = Image.FromFile("\\Images\\facebild.png");
+
             notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
             notifyIcon.Text = "No Mini EA";
             notifyIcon.ContextMenuStrip = new ContextMenuStrip();
-            notifyIcon.ContextMenuStrip.Items.Add("Exit", null, Exit_Click);
             notifyIcon.ContextMenuStrip.Items.Add("Auto start", null, startup.Auto_Start_Click);
+            notifyIcon.ContextMenuStrip.Items.Add("Exit", null, Exit_Click);
+            notifyIcon.MouseClick += NotifyIcon_MouseClick;
+        }
+
+        private void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left || e.Button == MouseButtons.Middle)
+            { notifyIcon.ContextMenuStrip.Show(Cursor.Position); }
         }
 
 
         private void Exit_Click(object sender, EventArgs e)
-        {Close();}
+        { Close(); }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-             Hide();
-             notifyIcon.Visible = true;
+            Hide();
+            notifyIcon.Visible = true;
             Timer1.Start();
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            Process[] processes = Process.GetProcessesByName("EADesktop");
+            Process[] processes = Process.GetProcessesByName(processName);
             foreach (Process process in processes)
             {
                 if (string.IsNullOrEmpty(process.MainWindowTitle) && process.MainWindowHandle == IntPtr.Zero)
-                  {
+                {
                     TimeSpan uptime = DateTime.Now - process.StartTime;
                     int desiredUptimeInSeconds = 11;
 
                     if (uptime.TotalSeconds >= desiredUptimeInSeconds)
-                    {ProcessKill();}
+                    { ProcessKill(); }
                 }
             }
         }
         private void ProcessKill()
         {
-            Process[] processes = Process.GetProcessesByName("EADesktop");
-            foreach (Process process in processes)
+            try
             {
-                try
+                Process[] processes = Process.GetProcessesByName(processName);
+
+                if (processes.Length > 0)
                 {
-                    process.Kill();
-                 //   Console.WriteLine($"Terminated process: {process.ProcessName} (ID: {process.Id})");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error terminating process: {ex.Message}");
+                    processes[0].Kill();
                 }
             }
-}
-}
+            catch (Win32Exception ex)
+            {
+                if (ex.NativeErrorCode != 0)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }   
+    }    
 }
 
